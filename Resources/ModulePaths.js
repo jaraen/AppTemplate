2ff8e76081a674263615173dc5ac1135d4d69f01
Javier Rayon <javier@criteriastudio.com>
2012-04-09 21:19:49 +0200

    conversion to CommonJS Modules

---
+++ Resources/ModulePaths.js
@@ -0,0 +1,12 @@
+
+var ui = '/app/ui/';
+
+module.exports = {
+	APPWINDOW: ui + 'AppWindow',
+	HELLOVIEW: ui + 'HelloWorldView',
+	
+	//Components
+	TOOLS: '/app/Tools',
+	STYLES: ui + 'Styles'
+	
+}
